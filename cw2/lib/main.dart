fdf2f06b8714ab30d2a830a45883611910acf098
ashwaqalh <112781588+ashwaqalh@users.noreply.github.com>
2022-09-12 14:44:02 -0600

    done

---
+++ cw2/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text('my first flutter app')),
+        body: Center(
+          child: Text(
+            'Ashwaq alharbi',
+            style: TextStyle(
+              color: Colors.blue,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
